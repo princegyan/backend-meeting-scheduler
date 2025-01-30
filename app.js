e90fdcedb3769749ff9f7f0adc9bf9a3456cf17d
princegyan <princealfredgyan@gmail.com>
2025-01-30 16:14:42 +0000

    Server New Update 2

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const meetingRoutes = require('./routes/meetings');  // Importing the meeting routes
+const connectDB = require('./config/db');  // Importing the database connection method
+
 const app = express();
 
-const port = process.env.PORT || 8080;
+// Middleware
+app.use(cors());  // Enable Cross-Origin Resource Sharing (CORS)
+app.use(bodyParser.json());  // Middleware to parse JSON request bodies
 
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Alfred Gyan");
 });
 
 app.listen(port, () => {
   `Server started on port ${port}`;
+});
+
+// Server setup
+const PORT = process.env.PORT || 8080;  // Use the specified PORT or default to 5000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);  // Start the server and log to the console
 });
