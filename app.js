fbbb46f6fe4206a765eb5d8dc6175f5ee1e12139
princegyan <princealfredgyan@gmail.com>
2025-01-30 16:04:16 +0000

    Server New Update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Subscribe to Arpan Neupane's channel");
+  res.send("Alfred Gyan");
 });
 
 app.listen(port, () => {
